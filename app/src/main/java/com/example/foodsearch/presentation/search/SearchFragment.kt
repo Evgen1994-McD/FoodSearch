eb1ddb5279f5f3a102ee96540e8462a78247cef1
Evgen1994-McD <mrmarwell@gmail.com>
2025-09-18 23:26:02 +0500

    Ответ от Api есть, поиск работает

--- app/src/main/java/com/example/foodsearch/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/example/foodsearch/presentation/search/SearchFragment.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -156,7 +157,7 @@ pbs = binding.pbs
 
 
             private fun displayRecipes(recipes: List<Recipe>) = with(binding) {
-                id.text = recipes.size.toString()
+                id.text = recipes[0].title.toString()
 //
 //        recyclerView.layoutManager = LinearLayoutManager(requireContext())
 //        recyclerView.adapter = TrackAdapter(tracks, this)
