7e4b2674f7028d5219b9bd64bb983032dd80400b
Evgen1994-McD <mrmarwell@gmail.com>
2025-09-20 23:32:53 +0500

    update

--- app/src/main/java/com/example/foodsearch/data/search/dto/details/RecipeDetailsResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.foodsearch.data.search.dto.details
-
-import com.example.foodsearch.data.search.dto.Response
-
-class RecipeDetailsResponse(
-
-    val recipe: RecipeDetailsDto
-) : Response()
