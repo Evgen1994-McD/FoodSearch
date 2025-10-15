90b6527689b62b07043653c2a8f4204cb6b90eed
Evgen1994-McD <mrmarwell@gmail.com>
2025-10-15 19:07:24 +0500

    апдейт

--- app/src/main/java/com/example/foodsearch/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/foodsearch/presentation/main/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
